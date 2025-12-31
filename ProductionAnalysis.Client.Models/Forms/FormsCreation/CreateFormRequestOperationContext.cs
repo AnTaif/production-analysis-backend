6a8f80b87f3bb4122afbc6a4761941bcfdcf1687
Mikhail Gavrilyak <gavrilyak.mikhail@gmail.com>
2026-01-01 04:56:10 +0500

    docs

--- ProductionAnalysis.Client.Models/Forms/FormsCreation/CreateFormRequestOperationContext.cs
+++ ProductionAnalysis.Client.Models/Forms/FormsCreation/CreateFormRequestOperationContext.cs
@@ -1,5 +1,12 @@
 ﻿namespace ProductionAnalysis.Client.Models.Forms.FormsCreation;
 
+/// <summary>
+/// Контекст создания формы для операции.
+/// Используется для операций, которые не требуют дополнительных параметров производства.
+/// </summary>
+/// <remarks>
+/// Этот тип контекста используется, когда форма создается для операции без специфических параметров продукта.
+/// </remarks>
 public class CreateFormRequestOperationContext : CreateFormRequestContextBase
 {
 }
