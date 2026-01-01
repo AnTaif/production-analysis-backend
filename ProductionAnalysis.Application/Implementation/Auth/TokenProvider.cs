82808b971aafd15106f042273dd51cd10663f2f0
Mikhail Gavrilyak <gavrilyak.mikhail@gmail.com>
2026-01-02 04:18:57 +0500

    убрал лишние поля из jwt

--- ProductionAnalysis.Application/Implementation/Auth/TokenProvider.cs
+++ ProductionAnalysis.Application/Implementation/Auth/TokenProvider.cs
@@ -52,13 +52,11 @@ public class JwtTokenProvider(IOptions<JwtOptions> options) : ITokenProvider
     {
         var claims = new List<Claim>
         {
-            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
             new(JwtRegisteredClaimNames.GivenName, user.FirstName),
             new(JwtRegisteredClaimNames.FamilyName, user.LastName),
             new(JwtRegisteredClaimNames.MiddleName, user.MiddleName ?? ""),
             new(JwtRegisteredClaimNames.Email, user.Email),
             new(JwtRegisteredClaimNames.Sid, user.Id.ToString()),
-            new(JwtRegisteredClaimNames.Sub, user.Email),
         };
         claims.AddRange(user.Roles.Select(role => new Claim(ClaimTypes.Role, role)));
 
