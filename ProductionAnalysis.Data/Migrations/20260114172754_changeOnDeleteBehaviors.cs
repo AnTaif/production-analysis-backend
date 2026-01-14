9cc1af5629542f92c8530718544a96f38b0306e6
Mikhail Gavrilyak <gavrilyak.mikhail@gmail.com>
2026-01-14 22:28:15 +0500

    add migration

---
+++ ProductionAnalysis.Data/Migrations/20260114172754_changeOnDeleteBehaviors.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductionAnalysis.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeOnDeleteBehaviors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_forms_AspNetUsers_CreatorId",
+                table: "forms");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_forms_AspNetUsers_LastEditorId",
+                table: "forms");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_forms_departments_DepartmentId",
+                table: "forms");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_forms_employees_ExecutorId",
+                table: "forms");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_forms_shifts_ShiftId",
+                table: "forms");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_forms_AspNetUsers_CreatorId",
+                table: "forms",
+                column: "CreatorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_forms_AspNetUsers_LastEditorId",
+                table: "forms",
+                column: "LastEditorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_forms_departments_DepartmentId",
+                table: "forms",
+                column: "DepartmentId",
+                principalTable: "departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_forms_employees_ExecutorId",
+                table: "forms",
+                column: "ExecutorId",
+                principalTable: "employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_forms_shifts_ShiftId",
+                table: "forms",
+                column: "ShiftId",
+                principalTable: "shifts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_forms_AspNetUsers_CreatorId",
+                table: "forms");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_forms_AspNetUsers_LastEditorId",
+                table: "forms");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_forms_departments_DepartmentId",
+                table: "forms");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_forms_employees_ExecutorId",
+                table: "forms");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_forms_shifts_ShiftId",
+                table: "forms");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_forms_AspNetUsers_CreatorId",
+                table: "forms",
+                column: "CreatorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_forms_AspNetUsers_LastEditorId",
+                table: "forms",
+                column: "LastEditorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_forms_departments_DepartmentId",
+                table: "forms",
+                column: "DepartmentId",
+                principalTable: "departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_forms_employees_ExecutorId",
+                table: "forms",
+                column: "ExecutorId",
+                principalTable: "employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_forms_shifts_ShiftId",
+                table: "forms",
+                column: "ShiftId",
+                principalTable: "shifts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
