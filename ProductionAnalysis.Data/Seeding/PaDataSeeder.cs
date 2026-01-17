51f2db256e2c00fc432c1427d4f0416f79c40717
Mikhail Gavrilyak <gavrilyak.mikhail@gmail.com>
2026-01-17 23:30:40 +0500

    seed: patch shift name

--- ProductionAnalysis.Data/Seeding/PaDataSeeder.cs
+++ ProductionAnalysis.Data/Seeding/PaDataSeeder.cs
@@ -560,7 +560,7 @@ public class PaDataSeeder(
         dbContext.Shifts.AddRange(
             new ShiftDbo { Id = 1, Name = "1", StartTime = new TimeOnly(8, 0) },
             new ShiftDbo { Id = 2, Name = "2", StartTime = new TimeOnly(16, 0) },
-            new ShiftDbo { Id = 3, Name = "3 (ночная)", StartTime = new TimeOnly(0, 0) }
+            new ShiftDbo { Id = 3, Name = "3", StartTime = new TimeOnly(0, 0) }
         );
 
         return Task.CompletedTask;
